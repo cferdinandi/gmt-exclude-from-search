28e3af348926bdbc3907b771d22440c64780bba9
Chris Ferdinandi <hello@gomakethings.com>
2013-10-16 17:25:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Exclude from Search
+A simple plugin to exclude pages from your WordPress search results. Forked from [C. Bavota](http://bavotasan.com/2010/excluding-pages-from-wordpress-search/) and [SpeckyBoy](http://speckyboy.com/2010/09/19/10-useful-wordpress-search-code-snippets/).
+
+## How It Works
+To get started with Exclude from Search, [view the online tutorial](http://cferdinandi.github.com/exclude-from-search/).
+
+
+## Changelog
+* v1.0 (October 16, 2013)
+  * Initial release.
+
+
+## License
+Exclude from Search is free to use under the [MIT License](http://gomakethings.com/mit/).
---
+++ exclude-from-search.php
@@ -0,0 +1,39 @@
+<?php
+
+/* ======================================================================
+
+	Plugin Name: Exclude from Search
+	Plugin URI: http://cferdinandi.github.io/exclude-from-search/
+	Description: Exclude pages from your WordPress search results.
+	Version: 1.0
+	Author: Chris Ferdinandi
+	Author URI: http://gomakethings.com
+	License: MIT
+
+	Forked from C. Bavota and SpeckyBoy.
+	http://bavotasan.com/2010/excluding-pages-from-wordpress-search/
+	http://speckyboy.com/2010/09/19/10-useful-wordpress-search-code-snippets/
+
+ * ====================================================================== */
+
+// Get settings
+require_once( dirname( __FILE__) . '/exsearch-options.php' );
+
+function exsearch_exclude_from_search( $query ) {
+	if ( $query->is_search ) {
+		$hide_all_pages = exsearch_get_exclude_all_pages();
+		$hide_specific_pages = exsearch_get_pages_to_exclude();
+		$pages_to_hide = explode( ',', $hide_specific_pages );
+
+		// Set query arguments based on user settings
+		if ( $hide_all_pages == 'on' ) {
+			$query->set( 'post_type', 'post' );
+		}
+		// $query->set( 'post__not_in', array( 2237,4084 ) );
+		$query->set( 'post__not_in', $pages_to_hide );
+	}
+	return $query;
+}
+add_filter( 'pre_get_posts', 'exsearch_exclude_from_search' );
+
+?>
